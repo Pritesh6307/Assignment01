42b0fa64bc0bd12aee162fc5914fc98fbdcf564f
HP <HP@DESKTOP-U7P4RJN>
2024-11-05 08:45:30 +0530

    Practice files

---
+++ src/firstAssignment/HelloWorld.java
@@ -0,0 +1,14 @@
+package firstAssignment;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int a = 1;
+		char ch = 'A';
+		System.out.println(a);
+		System.out.println(ch);
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ src/practicefiles/PracticeOne.java
@@ -0,0 +1,18 @@
+package practicefiles;
+
+import java.util.Scanner;
+
+public class PracticeOne {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first Number");
+		int a = sc.nextInt();
+		System.out.println("Enter second Number");
+		int b = sc.nextInt();
+		int Addition = a + b;
+		System.out.println("addition = " + Addition);
+
+	}
+
+}
